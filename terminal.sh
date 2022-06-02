86b961f7accc3bade9d8375053d06f520c126489
Ali Amori kadhim <ali@Alis-MBP.home>
2022-06-02 12:11:40 +0200

    setup new git config

---
+++ terminal.sh
@@ -0,0 +1,7 @@
+git config --global push.default current
+
+GIT_AUTHOR_NAME="Ali Amori Kadhim"
+GIT_AUTHOR_EMAIL="ali@frameless.io"
+
+git config --global user.name "$GIT_AUTHOR_NAME"
+git config --global user.email "$GIT_AUTHOR_EMAIL"
